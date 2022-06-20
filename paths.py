41123bed73900b60f0cfaef8669b7be0123cd0b0
derpinyou <55846013+derpinyou@users.noreply.github.com>
2022-06-20 20:54:55 +0300

    Add files via upload

---
+++ paths.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from pathlib import Path
+
+
+MODULE_DIR = Path(file).absolute().parent
+BASE_DIR = MODULE_DIR.parent
+DATA_DIR = BASE_DIR / "data"
+
